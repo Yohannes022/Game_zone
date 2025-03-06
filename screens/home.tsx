03cf43c05422af4976b73fcb0fa08f90026c8ea6
Yohannes <johannest022@gmail.com>
2025-03-06 17:17:16 +0300

    first commit

---
+++ screens/home.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text> Home 👋👋</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 40,
+    }
+})
